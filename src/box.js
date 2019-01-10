ab144fce3385a9d1a6ce4414f8245899a1a7a47b
badal2014 <badalhuria8@gmail.com>
2019-01-10 11:16:21 +0530

    first commit

---
+++ src/box.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Box extends React.Component{
+
+    render(){
+        return(
+            <div>Hello from box</div>
+        )
+    }
+}
+
+export default Box;
