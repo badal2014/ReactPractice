2a1ef1835d8995d5c120b3394c13b031b4122b5a
badal2014 <badalhuria8@gmail.com>
2019-02-04 23:27:46 +0530

    new push test

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends React.Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Leabadalt
           </a>
         </header>
         <Box/>
